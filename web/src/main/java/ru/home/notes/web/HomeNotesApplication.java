c8d384b1065d02b1329b6162881a57244ec8e100
zaytsev-dv <zaytsev.dmitry9228@gmail.com>
2020-10-26 19:51:41 +0300

    add test endpoints

--- web/src/main/java/ru/home/notes/web/HomeNotesApplication.java
+++ web/src/main/java/ru/home/notes/web/HomeNotesApplication.java
@@ -2,8 +2,10 @@ package ru.home.notes.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"ru.home.notes.*"})
 public class HomeNotesApplication
 {
 
